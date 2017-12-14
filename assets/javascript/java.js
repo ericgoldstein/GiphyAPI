e321c0ac2b0bce6fd5adbe26d7d8f102e71aed2d
ericgoldstein <bergerd306@aol.com>
2017-12-13 18:18:14 -0700

    super close to done

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var actions = ["Falling", "Laughing", "Running", "Eating", "Crying", "Drinking", "Sleeping", "Flying"];
 function displayGifButtons() {
-	$("#displayGifButtons").empty();
+	$("#gifButtonsView").empty();
 	for (var index = 0; index < actions.length; index++) {
 		var gifButton = $("<button>");
 		gifButton.addClass("action")
@@ -12,25 +12,27 @@ function displayGifButtons() {
 	}
 }
 function addNewButton() {
-	$("#addGif").on("click", function() {
+	$("#addGif").on("click", function(event) {
+		event.preventDefault();
 		var action = $("#action-input").val().trim();
 		if (action == "") {
 			return false;
 		}
-		action.push(action);
+		actions.push(action);
 		displayGifButtons();
 		return false;
 	});
 }
 function removedLastButton() {
-	$("#removeGif").on("click", function() {
-		actions.pop(action);
+	$("#removeGif").on("click", function(event) {
+		event.preventDefault();
+		actions.pop();
 		displayGifButtons();
 		return false;
 	});
 }
-function displayGifs() {
-	var action = $(this).attr("data-name");
+function displayGifs(action) {
+	console.log(action);
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
 	console.log(queryURL);
 	$.ajax({
@@ -52,7 +54,7 @@ function displayGifs() {
 		var gifImage = $("<img>");
 		gifImage.attr("src", results[index].images.fixed_height_small_still.url);
 		gifImage.attr("data-still", results[index].images.fixed_height_small_still.url);
-		gifImage.attr("data-animate", results[index].images.fixed_height_small_still.url);
+		gifImage.attr("data-animate", results[index].images.fixed_height_small.url);
 		gifImage.attr("data-state", "still");
 		gifImage.addClass("image");
 		gifDiv.append(gifImage);
@@ -65,15 +67,17 @@ displayGifButtons();
 addNewButton();
 removedLastButton();
 $(document).on("click", ".action", function() {
-	displayGifs();
+
+	
+	displayGifs($(this).attr("data-name"));
 });
 $(document).on("click", ".image", function() {
 	var state = $(this).attr('data-state');
-	if ( state == ' still'){
+	if ( state === 'still'){
 		$(this).attr('src', $(this).data('animate'));
 		$(this).attr('data-state', 'animate');
 	}else{
-		$(this).attr('src', $(this).data('animate'));
+		$(this).attr('src', $(this).data('still'));
 		$(this).attr('data-state', 'still');
 	}
 });
